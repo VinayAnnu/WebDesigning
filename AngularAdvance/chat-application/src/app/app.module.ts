1fac58befcb32e53287c2eb77e7ee7f1b2e95fd4
Vinay kumar ranjan <vinaykumarranjan83@gmail.com>
2019-09-22 09:28:10 +0530

    ChatApp Commit

--- AngularAdvance/chat-application/src/app/app.module.ts
+++ AngularAdvance/chat-application/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChatModule,
+    UserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent, pathMatch: 'full'},
+      { path: '', redirectTo: 'login', pathMatch: 'full'},
+      { path: '*', component: LoginComponent },
+      { path: '**', component: LoginComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularAdvance/chat-application/src/app/user/user.module.ts
+++ AngularAdvance/chat-application/src/app/user/user.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'sign-up', component: SignupComponent}
+    ])
   ]
 })
 export class UserModule { }
